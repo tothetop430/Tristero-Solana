9a3f686902cf0f9f1818f6ef5dd531b08e17391b
tothetop430 <jhuseyrer355545@gmail.com>
2024-11-19 21:43:03 +0300

    update

---
+++ test-python/endpoint/program_id.py
@@ -0,0 +1,3 @@
+from solders.pubkey import Pubkey
+
+PROGRAM_ID = Pubkey.from_string("76y77prsiCMvXMjuoZ5VRrhG5qYBrUMYTE5WgHqgjEn6")
---
+++ test-python/tristero/program_id.py
@@ -0,0 +1,3 @@
+from solders.pubkey import Pubkey
+
+PROGRAM_ID = Pubkey.from_string("E2okPYndsWqtniTNnoK2YHdZUwMEWpN1PtPW3woaY5Lm")
